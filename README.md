fac894b299637cbd96cfa901d28039a1033c7e9a
yuli-r <iuliana.rotaru234@gmail.com>
2023-02-13 19:39:34 +0000

    Reset files

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # tic-tac-toe
+
 <a href="https://www.flaticon.com/free-icons/tic-tac-toe" title="tic tac toe icons">Tab icon source</a>
---
+++ script.js
@@ -0,0 +1,7 @@
+//gameboard = ['x','x','o','o','o','x','o','x','o']
+//gameboard module
+//screenController module
+//player 1 & 2 = factories
+
+//on cell click handleClick, {once:true} - will only ever fire it once
+
